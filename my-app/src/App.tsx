3771f7b5de02608179196c79bedb1fda2797d92b
react-developer <128214623+nft-react-developer@users.noreply.github.com>
2023-08-24 07:18:37 +0000

    fix

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,9 +3,12 @@ import './App.css';
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
+const imageMeme = 'https://pbs.twimg.com/media/B4n_bB0IAAAhw54.jpg';
+
 function App() {
   const releaseDate = new Date('2023-09-16').getTime();
   const diff = releaseDate - new Date().getTime();
+
   return (
     <div className="App">
       <div className='CountDown'>
@@ -21,7 +24,8 @@ function App() {
 class Completed extends Component {
   render() {
     return <div className='Complete'>
-      <div className='Complete-inner'><a className='Complete-inner-link'  href='https://orcade.io'>GO TO ORCADE.IO</a></div> 
+          <h2>No release</h2>
+          <img src={imageMeme}></img>
       </div>
   }
 }
