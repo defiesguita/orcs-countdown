cfd8aea664af4b31370ca53851b8a41054558cba
react-developer <128214623+nft-react-developer@users.noreply.github.com>
2023-10-30 09:23:39 +0000

    add new release date

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,17 +6,27 @@ import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 const imageMeme = 'https://pbs.twimg.com/media/B4n_bB0IAAAhw54.jpg';
 
 function App() {
-  const releaseDate = new Date('2023-09-16').getTime();
-  const diff = releaseDate - new Date().getTime();
+  const releaseDate = new Date('2024-04-26');
+  const releaseDateAsTime =releaseDate.getTime();
+  const diff = releaseDateAsTime - new Date().getTime();
 
   return (
-    <div className="App">
-      <div className='CountDown'>
-         <FlipClockCountdown className='flip-clock' to={new Date().getTime() + diff} >
-            <Completed />
-         </FlipClockCountdown>
+      <div className="App">
+        <div className='next-release'>
+          release date: <br />
+          {
+             releaseDate.toLocaleString(undefined, {year: 'numeric', month: '2-digit', day: '2-digit', weekday:"long"}) // 'Wednesday, 14/06/2023, 13:43:57'
+          }
+        </div>
+        <div className='CountDown'>
+          <FlipClockCountdown className='flip-clock' to={new Date().getTime() + diff} >
+              <Completed />
+          </FlipClockCountdown>
+        </div>
+        <footer className='footer fadeInUp'>
+          <p>Would you like to help me to buy more orcs? <strong>7W9sJg9t9RvGhhJkGK5f1jZxQJ19U3bWQ1yRbwALcEpG </strong></p>
+          </footer>
       </div>
-    </div>
   );
 }
 
